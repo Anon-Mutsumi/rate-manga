a9b15aa8ab53d27e632415ba4386d3eb876e2208
Anon-Mutsumi <anon.mutsumi@gmail.com>
2025-12-03 16:37:20 -0600

    Create credentials.py
    Credentials to authenticate the user to manga sites

---
+++ credentials.py
@@ -0,0 +1,7 @@
+mangadex_credentials = {
+    "grant_type": "password",
+    "username": "[Insert Username Here]",
+    "password": "[Insert Password Here]",
+    "client_id": "[Insert Client ID Here]",
+    "client_secret": "[Insert Client Secret Here]",
+}
